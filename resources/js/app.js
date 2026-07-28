5725b02f203500666d17411c5eb1475c7520881f
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2026-07-28 14:59:58 +0530

    worked on css and js add

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,2 @@
 //
+console.log("Please like and subscribe");
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,17 @@
-<h1>Welcome Page</h1>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite(['resources/css/app.css','resources/css/custom.css','resources/js/app.js'])
+
+</head>
+<body>
+    <h1 class="green-heading">Welcome Page</h1>
+    <h1 >Welcome Page</h1>
+    <h1 class="red-heading">Welcome Page</h1>
+
+</body>
+</html>
