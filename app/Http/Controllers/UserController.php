f82244b7c9b7f7d8fe317f155c57491f2588b2ba
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2026-08-02 13:23:10 +0530

    done with request

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,13 @@ class UserController extends Controller
 {
     //
     function addUser(Request $request){
-        return $request->all();
+        // return $request->method();
+        // return $request->only(['phone',"username","dob"]);
+        // return $request->except(['phone',"username","dob"]);
+        // return $request->input('phone');
+        // return $request->phone;
+        // return $request->has('mobile');
+        $data= $request->input('name',"guest");
+        return $data;
     }
 }
