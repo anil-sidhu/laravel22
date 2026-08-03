e922fe33f64bcde5e495f287ef1aacabbd162873
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2026-08-03 14:44:22 +0530

    working on validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,13 +8,20 @@ class UserController extends Controller
 {
     //
     function addUser(Request $request){
+
+    $request->validate([
+        "username"=>"required | min:3",
+        "phone"=>"required |  numeric | digits_between:3,12",
+        "skills"=>"required | array"
+
+    ]);
         // return $request->method();
         // return $request->only(['phone',"username","dob"]);
         // return $request->except(['phone',"username","dob"]);
         // return $request->input('phone');
         // return $request->phone;
         // return $request->has('mobile');
-        $data= $request->input('name',"guest");
-        return $data;
+        // $data= $request->input('name',"guest");
+        return $request;
     }
 }
--- resources/views/add-user.blade.php
+++ resources/views/add-user.blade.php
@@ -4,11 +4,24 @@
     <title>Add User Form</title>
 </head>
 <body>
+    <!-- @if($errors->any())
+    <ul>
+        @foreach($errors->all() as $error)
+        <li style="color:red" >{{$error}}</li>
+        @endforeach
+    </ul>
+    @endif -->
     <form action="/add-user" method="post">
         @csrf
     <input type="text" name="username" placeholder="enter user name">
+    @error("username")
+     <span style="color:red" >{{$message}}</span>
+    @enderror
     <br><br>
     <input type="phone" name="phone" placeholder="enter user phone">
+      @error("phone")
+     <span style="color:red" >{{$message}}</span>
+    @enderror
     <br><br>
     <input type="email" name="email" placeholder="enter user email">
     <br><br>
@@ -18,14 +31,17 @@
     <br>
     <input type="radio" name="gender" value="male" ><span>Male</span>
     <input type="radio" name="gender" value="female"><span>Female</span>
+      @error("gender")
+     <span style="color:red" >{{$message}}</span>
+    @enderror
 
     <br><br>
 
         <label for="">Skills</label>
     <br>
-    <input type="checkbox" name="skills[]" value="laravel"  ><span>Laravel</span>
-    <input type="checkbox" name="skills[]" value="react"><span>React</span>
-    <input type="checkbox" name="skills[]" value="angular"><span>Angular</span>
+    <input type="checkbox" name="skills" value="laravel"  ><span>Laravel</span>
+    <input type="checkbox" name="skills" value="react"><span>React</span>
+    <input type="checkbox" name="skills" value="angular"><span>Angular</span>
 
     <br><br>
     <select name="" id="">
