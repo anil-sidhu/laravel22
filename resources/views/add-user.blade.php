a756c49a15cf2ec7804e47f5e7ad5f02b7e351fc
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2026-08-04 19:22:35 +0530

    worked on

--- resources/views/add-user.blade.php
+++ resources/views/add-user.blade.php
@@ -13,24 +13,24 @@
     @endif -->
     <form action="/add-user" method="post">
         @csrf
-    <input type="text" name="username" placeholder="enter user name">
+    <input type="text" name="username" value="{{old('username')}}" placeholder="enter user name">
     @error("username")
      <span style="color:red" >{{$message}}</span>
     @enderror
     <br><br>
-    <input type="phone" name="phone" placeholder="enter user phone">
+    <input type="phone" value="{{old('phone')}}" name="phone" placeholder="enter user phone">
       @error("phone")
      <span style="color:red" >{{$message}}</span>
     @enderror
     <br><br>
-    <input type="email" name="email" placeholder="enter user email">
+    <input type="email" value="{{old('email')}}" name="email" placeholder="enter user email">
     <br><br>
-    <input type="date" name="dob">
+    <input type="date" value="{{old('dob')}}" name="dob">
     <br><br>
     <label for="">Gender</label>
     <br>
-    <input type="radio" name="gender" value="male" ><span>Male</span>
-    <input type="radio" name="gender" value="female"><span>Female</span>
+    <input type="radio" name="gender" {{old('gender')=="male"?'checked':''}} value="male" ><span>Male</span>
+    <input type="radio" name="gender" {{old('gender')=="female"?'checked':''}} value="female"><span>Female</span>
       @error("gender")
      <span style="color:red" >{{$message}}</span>
     @enderror
@@ -39,9 +39,9 @@
 
         <label for="">Skills</label>
     <br>
-    <input type="checkbox" name="skills[]" value="laravel"  ><span>Laravel</span>
-    <input type="checkbox" name="skills[]" value="react"><span>React</span>
-    <input type="checkbox" name="skills[]" value="angular"><span>Angular</span>
+    <input type="checkbox" name="skills[]" {{ in_array("laravel", old('skills',[]))?'checked' :''}} value="laravel"  ><span>Laravel</span>
+    <input type="checkbox" name="skills[]" {{ in_array("react", old('skills',[]))?'checked' :''}} value="react"><span>React</span>
+    <input type="checkbox" name="skills[]" {{ in_array("angular", old('skills',[]))?'checked' :''}} value="angular"><span>Angular</span>
 
     <br><br>
     <select name="" id="">
@@ -50,9 +50,9 @@
         <option value="delhi">Delhi</option>
     </select>
 <br><br>
-<input type="range" name="age" min="18" max="100" >
+<input type="range" name="age"value="{{old('age')}}"  min="18" max="100" >
 <br><br>
-<textarea name="bio" id=""></textarea>
+<textarea name="bio" id="">{{old('bio')}}</textarea>
 <br><br>
     <button>Add New User</button>
 
