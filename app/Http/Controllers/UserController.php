8b064cbab2b2ec3d8e48b370c4bebcfae5546132
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2026-08-11 16:30:53 +0530

    db-operations

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    //
+    function index(){
+        DB::connection()->getPdo();
+        $users=DB::select("select * from users");
+        return $users;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::view('/','welcome');
+Route::get('/',[UserController::class,'index']);
 
