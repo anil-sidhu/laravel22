260ad8214bf4c971883e8c83442d3061c5a05e33
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2026-08-12 22:31:51 +0530

    working

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,4 +13,41 @@ class UserController extends Controller
         $users=DB::select("select * from users");
         return $users;
     }
+
+    function save(){
+         DB::connection()->getPdo();
+        $users=DB::insert("insert into users (name,email, phone) values (?,?,?)",[
+            "tony","tony@test.com",1234566
+        ]);
+        if($users){
+            return "New user is added";
+        }else{
+             return "No New user is added";
+        }
+     
+    }
+
+    function update(){
+
+      DB::connection()->getPdo();
+        $users=DB::update("update users set name=?, email=?,phone=? where id =?",[
+            "tony stark","tony@stark.com",999,9
+        ]);
+        if($users){
+            return " user is updated";
+        }else{
+             return "No user updated";
+        }
+
+    }
+
+    function delete(){
+ DB::connection()->getPdo();
+        $users=DB::delete("delete from users where id =?",[9]);
+        if($users){
+            return " user is deleted";
+        }else{
+             return "No user deleted";
+        }
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 Route::get('/',[UserController::class,'index']);
+Route::get('/save',[UserController::class,'save']);
+Route::get('/update',[UserController::class,'update']);
+Route::get('/delete',[UserController::class,'delete']);
+
+
+
 
